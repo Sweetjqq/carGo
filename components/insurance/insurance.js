1ffb4800b17a0956cefcc24e22c627a485ec16c8
季俏俏 <qiaoqiao.ji@freemud.cn>
2021-04-07 23:51:19 +0800

    险种组件

---
+++ components/insurance/insurance.js
@@ -0,0 +1,76 @@
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    showInsurance: {
+      value: false,
+      type: Boolean
+    },
+    orInsuraData:{
+      value:[],
+      type:Array
+    },
+    selectedIns:{
+      value:[],
+      type:Array
+    }
+  },
+  observers: {
+    'orInsuraData,selectedIns': function(orInsuraData,selectedIns) {
+      console.log(orInsuraData,selectedIns,'2222')
+      if(orInsuraData.length>0&&selectedIns.length>0){
+        // selectedIns.forEach(item=>{
+        //   let inx = orInsuraData.findIndex(val => {
+        //      val.dictValue == item;
+        //   })
+        //   if(inx>=0){
+        //     orInsuraData[inx].isSelect=true;
+        //   }
+        // })
+        // this.setData({
+        //   orInsuraData
+        // })
+      }
+    }
+  },
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    move(){},
+    closeEvent(){
+      this.setData({
+        showInsurance:false
+      })
+    },
+    selectEvent(e){
+      const {inx}=e.currentTarget.dataset;
+      const {orInsuraData}=this.data;
+      this.setData({
+        [`orInsuraData[${inx}].isSelect`]:orInsuraData[inx].isSelect?false:true
+      })
+    },
+     saveTime(){
+      const {orInsuraData}=this.data;
+      let arr=[];
+      orInsuraData.forEach(item=>{
+        if(item.isSelect){
+          arr.push({
+            dictLabel:item.dictLabel,
+            dictValue:item.dictValue
+          })
+        }
+      })
+      this.closeEvent();
+      this.triggerEvent('saveInsurance',{
+        reInsData:arr
+      })
+     }
+  }
+})
--- utils/util.js
+++ utils/util.js
@@ -87,6 +87,22 @@ const countDownTime = time => {
     seconds
   }
 }
+
+const getTimeState = () => {
+  let timeNow = new Date();
+  let hours = timeNow.getHours();
+  let text = ``;
+  if (hours >= 0 && hours <= 10) {
+    text = `早上好!`;
+  } else if (hours > 10 && hours <= 14) {
+    text = `中午好!`;
+  } else if (hours > 14 && hours <= 18) {
+    text = `下午好!`;
+  } else if (hours > 18 && hours <= 24) {
+    text = `晚上好!`;
+  }
+  return text;
+}
 module.exports = {
   formatTime,
   isEmpty,
@@ -95,5 +111,6 @@ module.exports = {
   getRedis,
   delRedis,
   clearRedis,
-  countDownTime
+  countDownTime,
+  getTimeState
 }
