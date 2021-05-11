6a86289bd57e15945a507618628e2fe8ac6d9224
andy <andy.sun@ximalaya.com>
2021-05-11 14:38:36 +0800

    feat: 异常字段

--- pages/mine/components/rank/rank.js
+++ pages/mine/components/rank/rank.js
@@ -19,22 +19,26 @@ Component({
       type: Number,
       value: 0
     },
-    "isLine":{
-      type:Boolean,
-      value:false
+    "isLine": {
+      type: Boolean,
+      value: false
     }
   },
-  ready(){
-    let query=wx.createSelectorQuery().in(this);
-    query.select('#num').boundingClientRect((rect)=> {
+  ready() {
+    let query = wx.createSelectorQuery().in(this);
+    query.select('#num').boundingClientRect((rect) => {
       this.setData({
-        numWidth:rect.width*wx.pixelRatio
-      }) 
+        numWidth: rect.width * wx.pixelRatio
+      })
     }).exec();
   },
   observers: {
-    'allNum, currNum': function(allNum, currNum) {
-      let percentage = (currNum/allNum)*622;
+    'allNum, currNum': function (allNum, currNum) {
+      // 异常数据处理
+      if (currNum > allNum) {
+        currNum = allNum;
+      }
+      let percentage = (currNum / allNum) * 622;
       this.setData({
         percentage
       })
@@ -44,15 +48,15 @@ Component({
    * 组件的初始数据
    */
   data: {
-    percentage:0,
-    numWidth:10
+    percentage: 0,
+    numWidth: 10
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    numHandle(){
+    numHandle() {
 
     },
   }
